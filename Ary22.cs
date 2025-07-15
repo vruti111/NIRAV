45dcf2c583b9a9201a1c83b4dd87b28b1bc26bfb
Vrutii <vrutip670@gmail.com>
2025-07-15 21:08:50 +0530

    3x2

---
+++ Ary22.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace NIRAV
+{
+    public class Ary22
+    {
+        public static void Main(string[] args)
+        //3*2 ARRAY,2*3 array
+        {
+            int[,] ary = { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+            Console.WriteLine("3x2 2D array:");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 2; j++)  // columns
+                {
+                    Console.Write(ary[i, j] + " ");
+                }
+                Console.WriteLine();  // new line after each row
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+        
