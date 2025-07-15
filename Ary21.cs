de07e6bfe8aa04b303c1fc7403fb6bde258b74c0
Vrutii <vrutip670@gmail.com>
2025-07-15 21:05:32 +0530

    Add project files.

---
+++ Ary21.cs
@@ -0,0 +1,23 @@
+﻿
+
+using System;
+namespace NIRAV
+{
+    public class Ary21
+    {
+        public static void Main(string[] args)
+        {
+            // 2x2 array
+            int[,] ary = new int[2, 2];
+            ary[0, 0] = 1;
+            ary[0, 1] = 2;
+            ary[1, 0] = 3;
+            ary[1, 1] = 4; // ✅ fixed index
+
+            Console.WriteLine("First element: " + ary[0, 0]);
+        }
+    }
+}
+
+
+
